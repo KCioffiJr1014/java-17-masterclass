5b068b61643b0ddbd845eb524513069a8c1b2ced
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2023-06-27 19:32:49 -0400

    Formatting clean up for MegaBytesConverter

--- MegaBytesConverter.java
+++ MegaBytesConverter.java
@@ -20,15 +20,15 @@
 
  ********************************************************************************************************************/
 public class MegaBytesConverter {
-
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
         int megabytes = kiloBytes / 1024;
         int remainder = kiloBytes % 1024;
+
         if (kiloBytes < 0) {
             System.out.println("Invalid Value");
         }
         else {
             System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainder + " KB");
-        }   }
-
+        }
+    }
 }
